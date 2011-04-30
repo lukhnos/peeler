a840344b15f8885c6eaed2cc9b5d4d66bf3ed5cd
Lukhnos D. Liu <lukhnos@lukhnos.org>
2011-04-29 22:23:03 -0700

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,15 @@
+Peeler
+======
+
+A study project to collect information and knowledge of Modern Mandarin.
+
+
+Author
+------
+Lukhnos D. Liu (lukhnos {that symbol} lukhnos {point} org)
+
+
+Copyright
+---------
+
+This project is in the public domain.
---
+++ bpmf/csvgen.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include "OVCINDataTable.h"
+#include "Mandarin.h"
+
+using namespace Formosa::Mandarin;
+using namespace OpenVanilla;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        cerr << "usage: dbgen <bpmf.cin> <bpmf-ext.cin>" << endl;
+        return 1;
+    }
+    
+    OVCINDataTableParser p1;    
+    OVCINDataTable *b1 = 0;
+    b1 = p1.CINDataTableFromFileName(argv[1]);
+    if (!b1) {
+        cerr << "Invalid file or no such file: " << argv[1] << endl;
+        return 1;
+    }
+    
+    OVCINDataTableParser p2;
+    OVCINDataTable *b2 = 0;
+    b2 = p2.CINDataTableFromFileName(argv[2]);
+    if (!b2) {
+        cerr << "Invalid file or no such file: " << argv[2] << endl;
+    }
+    
+    OVWildcard w("*");
+    vector<pair<string, string> > allB1 = b1->findChardefWithWildcard(w);
+    vector<pair<string, string> > allB2 = b2->findChardefWithWildcard(w);
+    set<string> originalBig5Set;
+    
+    for (vector<pair<string, string> >::const_iterator i = allB1.begin(),
+        e = allB1.end() ; i != e ; ++i)
+    {
+        originalBig5Set.insert((*i).second);
+    }
+    
+    const BopomofoKeyboardLayout *kl =
+        BopomofoKeyboardLayout::StandardLayout();
+
+    for (vector<pair<string, string> >::const_iterator i = allB2.begin(),
+        e = allB2.end() ; i != e ; ++i)
+    {
+        BPMF bpmf = kl->syllableFromKeySequence((*i).first);
+        bool inBig5 = (originalBig5Set.find((*i).second) !=
+            originalBig5Set.end());
+        
+        cout << (*i).second << "," << bpmf.composedString() << ","
+            << bpmf.HanyuPinyinString(true, true) << ","
+            << (*i).first << ","
+            << (inBig5 ? "big5" : "ext")
+            << endl;
+    }
+    
+    
+    delete b1;
+    delete b2;
+    
+    return 0;
+}
